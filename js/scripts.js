05a3a46db98a5526e79ad56114b2db7d0f6b788f
eugene-sakwa <the.ardent.arian@gmail.com>
2024-12-18 18:46:46 +0100

    Build bar chart and area chart using apex chart library (WIP)

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,120 @@
 //SIDEBAR TOGGLE
 
 let sidebarOpen = false;
-const sidebar = document.getElementById('sidebar');
+const sidebar = document.getElementById("sidebar");
 
 const openSidebar = () => {
-  if( !sidebarOpen){
-    sidebar.classList.add('sidebar-responsive')
+  if (!sidebarOpen) {
+    sidebar.classList.add("sidebar-responsive");
     sidebarOpen = true;
   }
-}
+};
 
 const closeSidebar = () => {
-  if( sidebarOpen){
-    sidebar.classList.remove('sidebar-responsive')
+  if (sidebarOpen) {
+    sidebar.classList.remove("sidebar-responsive");
     sidebarOpen = false;
   }
-}
+};
+
+//CHARTS
+
+//Bar Chart
+
+var barChartOptions = {
+  series: [
+    {
+      data: [10, 8, 6, 4, 2],
+    },
+  ],
+  chart: {
+    type: "bar",
+    height: 350,
+    toolbar: {
+      show: false,
+    },
+  },
+  colors: ["#246dec", "#cc3c43", "#367952", "#f5b74f", "#4f35a1"],
+  plotOptions: {
+    bar: {
+      distributed: true,
+      borderRadius: 4,
+      borderRadiusApplication: "end",
+      horizontal: false,
+      columnWidth: "40%",
+    },
+  },
+  dataLabels: {
+    enabled: false,
+  },
+  legends: {
+    show: false,
+  },
+  xaxis: {
+    categories: ["Laptop", "Phone", "Monitor", "Headphones", "Camera"],
+  },
+  yaxis: {
+    title: {
+      text: "Count",
+    },
+  },
+};
+
+var barChart = new ApexCharts(
+  document.querySelector("#bar-chart"),
+  barChartOptions
+);
+barChart.render();
+
+// Area Chart
+
+var areaChartOptions = {
+  series: [
+    {
+      name: "Purchase Orders",
+      data: [31, 40, 28, 51, 42, 109, 100],
+    },
+    {
+      name: "Sales Orders",
+      data: [11, 32, 45, 32, 34, 52, 41],
+    },
+  ],
+  chart: {
+    height: 350,
+    type: "area",
+    toolbar: {
+      show: false,
+    },
+  },
+  colors: ["#4f35a1", "#246dec"],
+  dataLabels: {
+    enabled: false,
+  },
+  stroke: {
+    curve: "smooth",
+  },
+  labels: ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul"],
+  markers: {
+    size: 0,
+  },
+  yaxis: [
+    {
+      title: {
+        text: "Purchase Orders",
+      },
+    },
+    {
+      opposite: true,
+      title: {
+        text: "Sales Orders",
+      },
+    },
+  ],
+  tooltip: {
+    shared: true,
+    intersect: false,
+  },
+};
+
+var areaChart = new ApexCharts(document.querySelector("#area-chart"), areaChartOptions);
+areaChart.render();
